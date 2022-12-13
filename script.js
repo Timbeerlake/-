abfe3a385e49bccc41bdf334228f50e645b274b9
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-13 09:43:30 +0300

    коммит услодненного 7 урок

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-"use strict"
+const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+const day = document.getElementById("weekDays"); 
+const todayDay = new Date();
 
-const num = function () {
-  let a;
+const days = function () {
+    week.forEach(function(item, i)  {
+        let newdiv = document.createElement("div"); 
+        if (i === +todayDay.getDay()-1) { 
+            console.log(todayDay.getDay());
+            newdiv.classList.add("today"); 
+            newdiv.textContent = week[i]; 
+        }
+        if (item == "Суббота" || item == "Воскресенье") { 
+            newdiv.classList.add("weekends"); 
+            newdiv.textContent = week[i]; 
+        } else {
+            newdiv.textContent = week[i]; 
+        }
+        day.appendChild(newdiv); 
 
-  do {
-    a = +prompt("введите число ")
-  } while (!a)
-  return a
-}
-
-num()
+    });
+};
+days(); 
